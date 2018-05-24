d9be1590b7fc4c76e9234e19985e37da978e7bd1
Itay Schmidt <itayschmidt@gmail.com>
2018-05-24 22:51:57 +0300

    merge

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+    state: (state = {}) => state
 });
-
+//root reducer
 export default rootReducer;
